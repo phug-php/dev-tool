5e446ff2ea2ecd5cdecb9663334a39878db2e57f
Kyle <kylekatarnls@gmail.com>
2016-12-28 23:17:52 +0100

    Use fake phpunit process to create the coverage file

--- tests/Phug/DevTool/Command/CheckCommandTest.php
+++ tests/Phug/DevTool/Command/CheckCommandTest.php
@@ -34,22 +34,6 @@ class CheckCommandTest extends \PHPUnit_Framework_TestCase
         foreach (glob(__DIR__.'/../../../app/vendor/bin/*') as $file) {
             chmod($file, 0777);
         }
-        file_put_contents('coverage.xml', '<?xml version="1.0" encoding="UTF-8"?>
-        <coverage generated="1482856255">
-            <project timestamp="1482856255">
-                <package name="Phug\DevTool">
-                    <file name="src/Phug/DevTool/Application.php">
-                        <class name="Application" namespace="Phug\DevTool">
-                            <metrics complexity="26" methods="17" coveredmethods="17" conditionals="0" coveredconditionals="0" statements="49" coveredstatements="49" elements="66" coveredelements="66"/>
-                        </class>
-                        <line num="20" type="method" name="__construct" visibility="public" complexity="1" crap="1" count="1"/>
-                        <line num="22" type="stmt" count="1"/>
-                        <line num="24" type="stmt" count="1"/>
-                        <line num="25" type="stmt" count="1"/>
-                    </file>
-                </package>
-            </project>
-        </coverage>');
         $input = new StringInput('check');
         $buffer = new BufferedOutput();
         $app = new Application();
