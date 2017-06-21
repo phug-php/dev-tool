80de33026598360a58d02d139f82a7c6eb993885
KyleKatarn <kylekatarnls@gmail.com>
2017-06-21 13:54:46 +0200

    Fix unit tests

--- tests/Phug/DevTool/Command/UnitTestsRunCommandTest.php
+++ tests/Phug/DevTool/Command/UnitTestsRunCommandTest.php
@@ -5,7 +5,7 @@ namespace Phug\Test\DevTool;
 use Phug\DevTool\Application;
 use Phug\DevTool\Command\UnitTestsRunCommand;
 use Symfony\Component\Console\Input\StringInput;
-use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\Console\Output\NullOutput;
 
 /**
  * Class UnitTestsRunCommandTest.
@@ -37,7 +37,6 @@ class UnitTestsRunCommandTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @group i
      * @covers ::execute
      */
     public function testExecute()
@@ -50,22 +49,26 @@ class UnitTestsRunCommandTest extends \PHPUnit_Framework_TestCase
         }
         $coverageHtml = $app.DIRECTORY_SEPARATOR.'coverage';
         $coverageClover = $app.DIRECTORY_SEPARATOR.'coverage.xml';
-        if (file_exists($coverageClover)) {
-            unlink($coverageClover);
-        }
+        $coverageHtmlShellArg = escapeshellarg(addslashes($coverageHtml));
+        $coverageCloverShellArg = escapeshellarg(addslashes($coverageClover));
         $input = new StringInput(
             'unit-tests:run'.
             ' --coverage-text'.
-            ' --coverage-html='.escapeshellarg(addslashes($coverageHtml)).
-            ' --coverage-clover='.escapeshellarg(addslashes($coverageClover))
+            ' --coverage-html='.$coverageHtmlShellArg.
+            ' --coverage-clover='.$coverageCloverShellArg
         );
-        $buffer = new BufferedOutput();
+        $buffer = new NullOutput();
         $app = new Application();
         $app->setAutoExit(false);
+        ob_start();
         $code = $app->run($input, $buffer);
-        exit($buffer->fetch());
-        self::assertTrue(file_exists($coverageHtml));
-        self::assertTrue(file_exists($coverageClover));
+        $contents = ob_get_contents();
+        ob_end_clean();
+        self::assertSame(0, $code);
+        $data = json_decode($contents);
+        self::assertTrue($data->{'--coverage-text'});
+        self::assertSame($coverageHtml, $data->{'--coverage-html'});
+        self::assertSame($coverageClover, $data->{'--coverage-clover'});
         if (file_exists($coverageHtml)) {
             self::remove($coverageHtml);
         }
@@ -73,7 +76,5 @@ class UnitTestsRunCommandTest extends \PHPUnit_Framework_TestCase
             unlink($coverageClover);
         }
         chdir($cwd);
-
-        self::assertRegExp('/test/', $buffer->fetch());
     }
 }
